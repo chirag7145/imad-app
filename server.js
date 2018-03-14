c95d2de1ff6f11cdc48420e497fe758ff4218f8b
chirag7145 <33162097+chirag7145@users.noreply.github.com>
2018-03-14 14:11:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,105 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+  article_one : {
+    title: 'Article one',
+    heading: 'Article-1',
+    date: '5/4/30',
+    content: `    <div>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+    </div>    
+    <div>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+    </div>    
+    <div>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+    </div>    
+    <div>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+    </div>`
+},  
+  article_two : {
+    title: 'Article two',
+    heading: 'Article-2',
+    date: '9/4/20',
+    content: `    <div>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+    </div>    
+    <div>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+    </div>    
+    <div>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+    </div>    
+    <div>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+    </div>`
+},  
+  article_three : {
+    title: 'Article three',
+    heading: 'Article-3',
+    date: '7/9/20',
+    content: `    <div>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+    </div>    
+    <div>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+    </div>    
+    <div>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+    </div>    
+    <div>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+        <p>Hello boys what's up !! Hello boys what's up !! Hello boys what's up ! Hello boys what's up !! Hello boys what's up !! Hello boys what's up !</p>
+    </div>`
+}
+};
+function createTemplate(data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+var htmlTemplate = 
+`<html>
+    <head>
+        <title>${title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="ui/style.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <h1>${heading}</h1>
+        <span>
+            <button><a href = "Article-2">Article-2</a></button>
+            <button><a href = "Article-3">Article-3</a></button>
+        </span>
+        <hr>
+        <h4>${date} </h4>
+    ${content}
+    </body>
+</html>`;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/:articleName', function (req, res) {
+    // articleName == article-one
+    // articles[articleName] == {} content object for article one
+    var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
